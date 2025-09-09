6a0e749e5081e36ec90e15329a540308120cd046
Darosh Luco <darosh.luco@gmail.com>
2025-09-09 17:28:36 -0300

    cambio necesario para abrirlo con docker

--- src/backend/duocpoint/settings/base.py
+++ src/backend/duocpoint/settings/base.py
@@ -96,7 +96,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 # Configurar archivos estáticos del frontend
-STATICFILES_DIRS = [BASE_DIR.parent / "frontend/static"]
+STATICFILES_DIRS = [BASE_DIR.parent / "frontend"]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
